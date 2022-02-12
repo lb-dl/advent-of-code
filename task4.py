4738da92d420f65ee6750d01e15fd823a5f17c35
ikravche <ikravche@softserveinc.com>
2022-02-12 12:15:26 +0200

    change the logic, following the comments

--- task4.py
+++ task4.py
@@ -6,53 +6,53 @@ https://adventofcode.com/2021/day/4
 """
 
 
-from itertools import chain
-
-
 def read_file(path):
     with open(path, 'r') as file:
         # add lines without trailing '\n' and skip empty lines, e.g. '59 98 84 27 56\n' -> '59 98 84 27 56'
         lines = [l.rstrip() for l in file.readlines() if l.strip()]
-        first_line_of_num = [int(s) for s in lines[0].split(',')]
-        # turn strings into lists of numbers, e.g. '59 98 84 27 56' -> [59, 98, 84, 27, 56]
-        list_of_numbers = [[int(s) for s in str_.split()] for str_ in lines[1:]]
-        # creat a list of 'list_of_boards', consisting of 25 first_line_of_num
-        list_of_boards = []
-        for i in range(0, len(list_of_numbers), 5):
-            list_of_boards.append(list(chain.from_iterable(list_of_numbers[i:i + 5])))
-
-    return list_of_boards, first_line_of_num
+        return lines
 
 
-def is_a_winner(list_):
-    start = 0
-    for i in range(5):
-        if list_[i] + list_[i + 1] + list_[i + 2] + list_[i + 3] + list_[i + 4] == 500:
+def creat_list_of_rows(list_):
+    list_of_rows = [list_[i:i + 5] for i in range(0, len(list_), 5)]
+    return list_of_rows
+
+
+def swap_rows_with_columns(l):
+    l1 = [list(x) for x in zip(*l)]
+    return l1
+
+
+def is_a_winner(l, n, m):
+    for i in range(m):
+        if n in l[i]:
+            l[i].remove(n)
+        if not l[i]:
             return True
-        start += 5
-    for i in range(5):
-        if list_[i] + list_[i + 5] + list_[i + 10] + list_[i + 15] + list_[i + 20] == 500:
-            return True
-        start += 1
-    return False
 
 
-data = read_file('task4.txt')
-boards, numbers = data
-print(boards, numbers)
+list_of_lines = read_file('task4.txt')
+first_line_of_num = [int(s) for s in list_of_lines[0].split(',')]
+list_of_numbers = [[int(s) for s in str_.split()] for str_ in list_of_lines[1:]]
 
-flag = False
-while flag == False:
-    number = numbers[0]
-    numbers = numbers[1:]
+rows = creat_list_of_rows(list_of_numbers)
+columns = []
+for i in range(len(rows)):
+    columns.append(swap_rows_with_columns(rows[i]))
 
-    for board in boards:
-        for i in range(len(board)):
-            if board[i] == number:
-                board[i] = 100
 
-    for board in boards:
-        if is_a_winner(board):
-            sum_of_unmarked = sum([n for n in board if n != 100])
-            score = sum_of_unmarked * number
-            flag = True
+found_a_winner = False
+for n in first_line_of_num:
+    for i in range(len(rows)):
+        if is_a_winner(rows[i], n, 5):
+            score = sum([sum(n) for n in rows[i]])
+            total_score = score * n
+            print(f'total_score: {total_score}')
+            found_a_winner = True
+        elif is_a_winner(columns[i], n, 5):
+            score = sum([sum(n) for n in columns[i]])
+            total_score = score * n
+            print(f'total_score: {total_score}')
+            found_a_winner = True
+    if found_a_winner:
+        break
